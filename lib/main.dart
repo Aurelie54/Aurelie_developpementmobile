676a7f6cb2a58ecbc826259dba042967d46e2032
aurelie <aurelie.audemar@orange.fr>
2023-04-24 16:52:52 +0200

    Modification + amélioration

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MuseumApp extends StatelessWidget {
-  const MuseumApp({super.key});
+  const MuseumApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,9 +19,8 @@ class MuseumApp extends StatelessWidget {
   }
 }
 
-
 class ArtWork extends StatefulWidget {
-  const ArtWork({super.key, required this.title});
+  const ArtWork({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -30,73 +29,56 @@ class ArtWork extends StatefulWidget {
 }
 
 class _MyArtworkState extends State<ArtWork> {
-int count = 0;
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body:  Center(
+      body: Center(
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Image.asset('assets/Mona_Lisa.jpg'),
             const Text(
               'Mona Lisa',
-                style: TextStyle(
-                  fontFamily: 'Merriweather',
-                  fontSize: 30,
-                  color: Colors.brown,
-  ),
-  
+              style: TextStyle(
+                fontFamily: 'Merriweather',
+                fontSize: 30,
+                color: Colors.brown,
+              ),
             ),
-            const Text (
+            const Text(
               'Léonard De Vinci',
-                style: TextStyle(
-                  fontFamily: 'Merriweather',
-                  fontSize: 15,
-                  fontWeight : FontWeight.bold,
-                  color: Colors.brown,        
-  ),
+              style: TextStyle(
+                fontFamily: 'Merriweather',
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: Colors.brown,
+              ),
             ),
-//
-            //const IconButton(
-            //iconSize: 100,
-            //icon: Icon(
-              //Icons.favorite,
-            //),
-            
-           // onPressed: () {
-              //setState(
-              //  () {
-               //   count++;
-               // },
-              //);
-            //},
-            //),
-
-          //const IconButton(
-           // icon: Icon(
-              //Icons.article,
-            //),
-
-          const Positioned(
-          bottom: 15, right: 15, 
-          child: Icon(
-            Icons.favorite, 
-            color: Colors.white70, 
-            size:100),    
-
-          )
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.article),
+                  onPressed: () {
+                    // Action à effectuer lors du clic sur l'icône article
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.favorite),
+                  onPressed: () {
+                    // Action à effectuer lors du clic sur l'icône favorite
+                  },
+                ),
+              ],
+            ),
           ],
         ),
       ),
-
     );
   }
 }
-
-
