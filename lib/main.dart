57f895cf1e5e9009420935621c3a0add9cb8b775
aurelie <aurelie.audemar@orange.fr>
2023-04-25 11:24:07 +0200

    Apparition texte article bouton

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,8 @@ class ArtWork extends StatefulWidget {
 
 class _MyArtworkState extends State<ArtWork> {
   bool _isFavorite = false;
+  bool _showDescription = false;
+  
 
   @override
   Widget build(BuildContext context) {
@@ -61,27 +63,38 @@ class _MyArtworkState extends State<ArtWork> {
                     color: Colors.brown,
                   ),
                 ),
+                Visibility(
+                  visible: _showDescription,
+                  child: const Text(
+                    'La Joconde, ou Portrait de Mona Lisa, est un tableau de l\'artiste Léonard de Vinci, réalisé entre 1503 et 1506 ou entre 1513 et 15161,2, et peut-être jusqu\'à 1517 (l\'artiste étant mort le 2 mai 1519), qui représente un portrait mi-corps, probablement celui de la Florentine Lisa Gherardini, épouse de Francesco del Giocondo. Acquise par François Ier, cette peinture à l\'huile sur panneau de bois de peuplier de 77 × 53 cm est exposée au musée du Louvre à Paris. La Joconde est l\'un des rares tableaux attribués de façon certaine à Léonard de Vinci. La Joconde est devenue un tableau éminemment célèbre car, depuis sa réalisation, nombre d\'artistes l\'ont pris comme référence. À l\'époque romantique, les artistes ont été fascinés par ce tableau et ont contribué à développer le mythe qui l\'entoure, en faisant de ce tableau l’une des œuvres d\'art les plus célèbres du monde, si ce n\'est la plus célèbre : elle est en tout cas considérée comme l\'une des représentations d\'un visage féminin les plus célèbres au monde. Au xxie siècle, elle est devenue l\'objet d\'art le plus visité au monde, devant le diamant Hope, avec 20 000 visiteurs qui viennent l\'admirer et la photographier quotidiennement.',
+                    style: TextStyle(
+                      fontFamily: 'Merriweather',
+                      fontSize: 15,
+                      color: Colors.brown,
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
-          const Positioned(
+          Positioned(
             left: 0,
             right: 0,
             top: 0,
             bottom: 0,
             child: Opacity(
-              opacity: 0.75,
+              opacity: _isFavorite ? 1.0 : 0.75,
               child: Icon(
                 Icons.favorite,
-                color: Colors.white,
+                color: _isFavorite ? Colors.red : Colors.white,
                 size: 100,
               ),
             ),
           ),
           Positioned(
-            top: 20,
             left: 20,
             right: 20,
+            bottom: 15,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -90,17 +103,27 @@ class _MyArtworkState extends State<ArtWork> {
                 IconButton(
                   icon: const Icon(Icons.article),
                   onPressed: () {
+                      setState(() {
+                      _showDescription = !_showDescription;
+                    });
                     // Action à effectuer lors du clic sur l'icône article
                   },
                 ),
                 IconButton(
                   icon: Icon(
                     _isFavorite ? Icons.favorite : Icons.favorite_border,
-                    color: Colors.red,
+                    color: Colors.brown,
+                  
                   ),
                   onPressed: () {
                     setState(() {
                       _isFavorite = !_isFavorite;
+
+                      if (_isFavorite) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Oeuvre ajoutée à vos favoris')),
+                          );
+                           }
                     });
                   },
                 ),
