e74a370628d7b818edb5101e001d5e7cdc2e65f8
aurelie <aurelie.audemar@orange.fr>
2023-04-25 11:43:56 +0200

    Question 8 TP fini

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,10 @@ class _MyArtworkState extends State<ArtWork> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Image.asset('assets/Mona_Lisa.jpg'),
+                Image.asset('assets/Mona_Lisa.jpg',
+                width: 400,
+                height: 400,
+                ),
                 const Text(
                   'Mona Lisa',
                   style: TextStyle(
@@ -65,13 +68,26 @@ class _MyArtworkState extends State<ArtWork> {
                 ),
                 Visibility(
                   visible: _showDescription,
-                  child: const Text(
+                  child: Container(
+                    width: 300,
+                    height: 350,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: const SingleChildScrollView(
+                    child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                  child: Text(
                     'La Joconde, ou Portrait de Mona Lisa, est un tableau de l\'artiste Léonard de Vinci, réalisé entre 1503 et 1506 ou entre 1513 et 15161,2, et peut-être jusqu\'à 1517 (l\'artiste étant mort le 2 mai 1519), qui représente un portrait mi-corps, probablement celui de la Florentine Lisa Gherardini, épouse de Francesco del Giocondo. Acquise par François Ier, cette peinture à l\'huile sur panneau de bois de peuplier de 77 × 53 cm est exposée au musée du Louvre à Paris. La Joconde est l\'un des rares tableaux attribués de façon certaine à Léonard de Vinci. La Joconde est devenue un tableau éminemment célèbre car, depuis sa réalisation, nombre d\'artistes l\'ont pris comme référence. À l\'époque romantique, les artistes ont été fascinés par ce tableau et ont contribué à développer le mythe qui l\'entoure, en faisant de ce tableau l’une des œuvres d\'art les plus célèbres du monde, si ce n\'est la plus célèbre : elle est en tout cas considérée comme l\'une des représentations d\'un visage féminin les plus célèbres au monde. Au xxie siècle, elle est devenue l\'objet d\'art le plus visité au monde, devant le diamant Hope, avec 20 000 visiteurs qui viennent l\'admirer et la photographier quotidiennement.',
                     style: TextStyle(
                       fontFamily: 'Merriweather',
                       fontSize: 15,
                       color: Colors.brown,
                     ),
+                  ),
+                      ),
+                      ),
                   ),
                 ),
               ],
